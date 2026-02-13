abdc312369824b1774be5b20d2a4978c384bf1fc
Parisa Reza <rezaparisa5@gmail.com>
2026-02-13 13:47:01 +0600

    server : server configured

---
+++ server/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+const path = require ("path");
+
+const app = express();
+
+// middleware to handle CORS
+app.use(cors({
+    origin  : "*",
+    methods : ["GET" , "POST" , "PUT" , "DELETE"],
+    allowedHeaders : ["Content-Type", "Authorization"]
+}))
+
+
+// middleware
+app.use(express.json())
+
+//routes
+
+// serve uploads folder
+
+app.use ("/uploads" , express.static(path.join(__dirname, "uploads"), {}))
+
+//server start
+const PORT = process.env.PORT || 5000;
+app.listen( PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
