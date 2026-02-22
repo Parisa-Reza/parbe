8e4b8ee98f248c47522216cd53c1c7dc7feca384
Parisa Reza <rezaparisa5@gmail.com>
2026-02-23 03:01:23 +0600

    server : implemented  get all sessions, get session by id , delete session

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,4 +1,4 @@
-import {Question,Session }  from "../models/index.js";
+import { Question, Session } from "../models/index.js";
 export const createSession = async (req, res) => {
   try {
     const { role, experience, topicsToFocus, description, questions } =
@@ -36,6 +36,10 @@ export const createSession = async (req, res) => {
 
 export const getMySessions = async (req, res) => {
   try {
+    const sessions = await Session.find({ user: req.user.id })
+      .sort({ createdAt: -1 })
+      .populate("questions");
+    res.status(200).json(sessions);
   } catch (error) {
     res.status(500).json({ success: false, message: "server error" });
   }
@@ -43,6 +47,19 @@ export const getMySessions = async (req, res) => {
 
 export const getSessionById = async (req, res) => {
   try {
+    const session = await Session.findById(req.params.id)
+      .populate({
+        path: "questions",
+        options: { sort: { isPinned: -1, createdAt: 1 } },
+      })
+      .exec();
+
+    if (!session) {
+      return res
+        .status(404)
+        .json({ success: false, message: "session not found" });
+    }
+    res.status(200).json(session);
   } catch (error) {
     res.status(500).json({ success: false, message: "server error" });
   }
@@ -50,6 +67,25 @@ export const getSessionById = async (req, res) => {
 
 export const deleteSession = async (req, res) => {
   try {
+    const session = await Session.findById(req.params.id);
+    if (!session) {
+      return res
+        .status(404)
+        .json({ success: false, message: "session not found" });
+    }
+
+    if (session.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({
+          success: false,
+          message: "Not authorized to delete the session",
+        });
+    }
+
+    await Question.deleteMany({ session: session._id });
+    await session.deleteOne();
+    res.status(200).json({ message: "Session deleted successfully" });
   } catch (error) {
     res.status(500).json({ success: false, message: "server error" });
   }
