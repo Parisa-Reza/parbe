0a545a635c4e628f5780423be535d7d884a795a3
Parisa Reza <rezaparisa5@gmail.com>
2026-03-03 07:17:22 +0600

    server : added default cors()

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-
-import path from "path";
-import { fileURLToPath } from "url";
 import  {protect} from  "./middlewares/index.js"
 
 import connectDB from "./config/db.js";
@@ -13,13 +10,8 @@ import {generateInterviewQuestions,generateConceptExplanations} from "./controll
 dotenv.config();
 const app = express();
 
-// middleware to handle CORS
-app.use(cors({
-    origin  : "*",
-    methods : ["GET" , "POST" , "PUT" , "DELETE"],
-    allowedHeaders : ["Content-Type", "Authorization"]
-}))
 
+app.use(cors());
 
 // middleware
 app.use(express.json())
@@ -34,19 +26,6 @@ app.use("/api/question" ,questionRoutes)
 app.use("/api/ai/generate-questions" ,protect, generateInterviewQuestions)
 app.use("/api/ai/generate-explanation" ,protect, generateConceptExplanations)
 
-// server uploads folder
-
-// app.use ("/uploads" , express.static(path.join(__dirname, "uploads"), {}))
-
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "uploads"))
-);
 
 //server start
 const PORT = process.env.PORT || 5000;
